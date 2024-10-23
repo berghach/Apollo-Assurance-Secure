cc3202a6b4e66f06d591ef3a05e9b1b538421ff7
berghach <berghach.chemseddine@gmail.com>
2024-10-23 11:44:17 +0100

    AA-3 adding the annotations

--- src/main/java/repository/HealthContractRepository.java
+++ src/main/java/repository/HealthContractRepository.java
@@ -2,8 +2,10 @@ package repository;
 
 import entities.HealthContract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface HealthContractRepository extends JpaRepository<HealthContract, UUID> {
 }
